95e258f3b412d8ca11c4a93b25a47ca0d332b245
vidde <johanna.julen@gmail.com>
2015-09-03 15:31:15 +0200

    Fixed some missing variable errors

--- sample-custom_content/functions/meta.php
+++ sample-custom_content/functions/meta.php
@@ -61,44 +61,47 @@ function meta_get_description()
 				$this_side=$arr;
 		}
 	}
-	else
+	else if(isset($this_page))
 		$this_side=$this_page;
 	
 	if(isset($this_side['meta_description']))
 		return $this_side['meta_description'];
-	else if(!strcmp($_GET['p'],"feedback"))
+	else if(isset($_GET['p']))
 	{
-		return sprintf(_("Page on %s where you can leave feedback"),SITE_NAME);
-	}
-	else if(!strcmp($_GET['p'],"news"))
-	{
-		return sprintf(_("News on site %s"),SITE_NAME);
-	}
-	else if(!strcmp($_GET['p'],"user") && isset($_GET['s']) && !strcmp($_GET['s'],"profile"))
-	{
-		if(isset($_GET['user']))
-			$user=$_GET['user'];
-		else if(isset($_SESSION[PREFIX.'user_id']))
-			$user=$_SESSION[PREFIX.'user_id'];
-		if(isset($_user))
-			return sprintf(_("User %s on site %s. %s"),user_get_name($user), SITE_NAME, user_get_description($user));
-	}
-	else if(!strcmp($_GET['p'],"user") && isset($_GET['s']) && !strcmp($_GET['s'],"privmess"))
-	{
-		return sprintf(_("Private messages on site %s"),SITE_NAME);
-	}
-	// else if(!strcmp($_GET['p'],"usersettings"))
-	else if(!strcmp($_GET['p'],"user") && isset($_GET['s']) && !strcmp($_GET['s'],"settings"))
-	{
-		return sprintf(_("User settings on site %s"),SITE_NAME);
-	}
-	else if(!strcmp($_GET['p'],"admin"))
-	{
-		return sprintf(_("Admin tools on site %s"),SITE_NAME);
-	}
-	else if(!strcmp($_GET['p'],"changelog"))
-	{
-		return sprintf(_("Change log on site %s"),SITE_NAME);
+		if(!strcmp($_GET['p'],"feedback"))
+		{
+			return sprintf(_("Page on %s where you can leave feedback"),SITE_NAME);
+		}
+		else if(!strcmp($_GET['p'],"news"))
+		{
+			return sprintf(_("News on site %s"),SITE_NAME);
+		}
+		else if(!strcmp($_GET['p'],"user") && isset($_GET['s']) && !strcmp($_GET['s'],"profile"))
+		{
+			if(isset($_GET['user']))
+				$user=$_GET['user'];
+			else if(isset($_SESSION[PREFIX.'user_id']))
+				$user=$_SESSION[PREFIX.'user_id'];
+			if(isset($_user))
+				return sprintf(_("User %s on site %s. %s"),user_get_name($user), SITE_NAME, user_get_description($user));
+		}
+		else if(!strcmp($_GET['p'],"user") && isset($_GET['s']) && !strcmp($_GET['s'],"privmess"))
+		{
+			return sprintf(_("Private messages on site %s"),SITE_NAME);
+		}
+		// else if(!strcmp($_GET['p'],"usersettings"))
+		else if(!strcmp($_GET['p'],"user") && isset($_GET['s']) && !strcmp($_GET['s'],"settings"))
+		{
+			return sprintf(_("User settings on site %s"),SITE_NAME);
+		}
+		else if(!strcmp($_GET['p'],"admin"))
+		{
+			return sprintf(_("Admin tools on site %s"),SITE_NAME);
+		}
+		else if(!strcmp($_GET['p'],"changelog"))
+		{
+			return sprintf(_("Change log on site %s"),SITE_NAME);
+		}
 	}
 	
 	return SELLING_TEXT;
