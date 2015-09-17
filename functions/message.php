a05e910de62e1876a5ca14514820532a4c75c077
vidde <johanna.julen@gmail.com>
2015-09-17 22:05:15 +0200

    progress bar function

--- functions/message.php
+++ functions/message.php
@@ -75,4 +75,12 @@ function add_message($message)
 	}
 }
 
+function message_progress_bar($percent)
+{
+	return '<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: '.$percent.'%;">
+    '.$percent.'%
+  </div>
+</div>';
+}
 ?>
