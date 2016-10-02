4075b15cfff615748713f367cdc60ae889e38d90
vidde <johanna.julen@gmail.com>
2016-10-02 09:42:08 +0200

    added function for getting title from url

--- functions/string.php
+++ functions/string.php
@@ -21,6 +21,16 @@ function password_generate($len)
 	return $pass;
 }
 
+function string_get_title_from_url($url)
+{
+  $str = file_get_contents($url);
+  if(strlen($str)>0){
+    $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
+    preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
+    return $title[1];
+  }
+}
+
 function curPageURL() {
  $pageURL = 'http';
  if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
