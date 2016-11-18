3780d126ba00cb7be196d3b89c2765b58248ff7d
vidde <johanna.julen@gmail.com>
2016-11-18 07:26:13 +0100

    message_try_mysql now returns affected rows if no insert id is available (like for UPDATE operations)

--- functions/message.php
+++ functions/message.php
@@ -67,7 +67,9 @@ function message_try_mysql($sql,$error_code, $success_message=NULL, $print_now=F
 			else
 				message_add_success_message($success_message);
 		}
-		return mysql_insert_id();
+		$insert_id=mysql_insert_id();
+		$affected_rows=mysql_affected_rows();
+		return ($insert_id ? $insert_id: $affected_rows);
 	}
 	else
 	{
