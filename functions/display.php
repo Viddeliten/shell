15bc58f8d50b2c913841d7afb6efc39ca83e8db5
vidde <johanna.julen@gmail.com>
2016-11-23 19:44:34 +0100

    allow url as target in top menu

--- functions/display.php
+++ functions/display.php
@@ -129,33 +129,6 @@ function display_custom_pages_menu()
 {
 	$custom_pages=unserialize(CUSTOM_PAGES_ARRAY);
 	display_menu_pages($custom_pages);
-/* 	$logged_in_level=login_check_logged_in_mini();
-	
-	// echo "<pre>".print_r($custom_pages,1)."</pre>";
-	
-	foreach($custom_pages as $name => $content)
-	{
-		if((!isset($content['req_user_level']) || $content['req_user_level']<1 || $logged_in_level>=$content['req_user_level']) && strcmp($content['slug'],"admin"))
-		{
-			if(!isset($content['subpages']) || empty($content['subpages']))
-			{
-				echo '<li ><a href="'.SITE_URL.'/?p='.$content['slug'].'" >'._($name).'</a></li>';
-			}
-			else
-			{
-				echo '<li class="dropdown">
-					  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">'._($name).'<span class="caret"></span></a>
-					  <ul class="dropdown-menu" role="menu">';
-					  foreach($content['subpages'] as $s_name => $s_content)
-					  {
-							if(!isset($s_content['req_user_level']) || $s_content['req_user_level']<1 || $logged_in_level>=$s_content['req_user_level'])
-								echo '<li ><a href="'.SITE_URL.'/?p='.$content['slug'].'&amp;s='.$s_content['slug'].'" >'._($s_name).'</a></li>';
-					  }
-				echo '</ul>
-					</li>';
-			}
-		}
-	} */
 }
 
 function display_menu_pages($custom_pages)
@@ -168,7 +141,11 @@ function display_menu_pages($custom_pages)
 	{
 		if((!isset($content['req_user_level']) || $content['req_user_level']<1 || $logged_in_level>=$content['req_user_level']) && strcmp($content['slug'],"admin"))
 		{
-			if(!isset($content['subpages']) || empty($content['subpages']))
+			if(isset($content['url']))
+			{
+				echo html_tag("li",html_link($content['url'], _($name)));
+			}
+			else if(!isset($content['subpages']) || empty($content['subpages']))
 			{
 				echo '<li ><a href="'.SITE_URL.'/?p='.$content['slug'].'" >'._($name).'</a></li>';
 			}
