468950fc8b2b2672c82c3dfa129e06d3876cf4f2
vidde <johanna.julen@gmail.com>
2017-03-20 13:51:01 +0100

    lowered default feedbacks returned from api

--- functions/api.php
+++ functions/api.php
@@ -32,7 +32,7 @@ if(isset($_REQUEST['p']) && isset($_REQUEST['s']) && !strcmp(strtolower($_REQUES
 
 function api_feedback()
 {
-	$feedback=feedback_get_array((isset($_REQUEST['from']) ? $_REQUEST['from'] : 0), (isset($_REQUEST['to']) ? $_REQUEST['to'] : 10));
+	$feedback=feedback_get_array((isset($_REQUEST['from']) ? $_REQUEST['from'] : 0), (isset($_REQUEST['to']) ? $_REQUEST['to'] : 5));
 	echo json_encode($feedback);
 }
 ?>
