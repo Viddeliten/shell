2bdad754064db0b5bdd366819bda13ca4cf09cfb
Vidde <johanna.julen@gmail.com>
2019-03-13 21:44:10 +0100

    added function nightbot_clear_playlist

--- functions/nightbot.php
+++ functions/nightbot.php
@@ -31,4 +31,19 @@ function nightbot_get_playlist($user_id=NULL)
 	
 	return $playlist;
 }
+
+function nightbot_clear_playlist($user_id=NULL)
+{
+	// if no user id was sent to function, fetch user id of current logged in user
+	if($user_id==NULL)
+		$user_id=login_get_user();
+	
+	// Just stop if we have no user id at this point
+	if($user_id==NULL)
+		return FALSE;
+	
+	// Clear playlist
+	$api=new rest_api_integration("nightbot", TRUE);
+	$api->_delete(array("1","song_requests","playlist"));
+}
 ?>
